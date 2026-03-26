8792b110d8bf438625b35ce1cbfd19dabf4b0e50
Prahlad Kumar <prahladchoudhary7980@gmail.com>
2026-03-26 21:05:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.addEventListener("mousemove", (e) => {
+//     const x = e.clientX;
+//     const y = e.clientY;
+
+//     document.body.style.background = `
+//         radial-gradient(
+//             350px circle at ${x}px ${y}px,
+//             rgba(0,245,196,0.06),
+//             rgba(10,10,10,1) 70%
+//         )
+//     `;
+// });
